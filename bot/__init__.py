2070b674221e6f9a6cf5893adf25f88ac2b2f937
MahinHaqqani <mahinhaquani@gmail.com>
2025-08-04 01:39:27 +0400

    Update __init__.py

---
+++ bot/__init__.py
@@ -0,0 +1,8 @@
+# bot/__init__.py
+
+"""
+VigilantEye Drug Detection Bot Package
+This module initializes the bot package.
+"""
+
+# This can be empty or used for future package-level initializations.
